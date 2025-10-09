883105061a44af5079fde44ddcd40eea30d9a3aa
sailesh123kumar <saileshkumar17893@gmail.com>
2025-10-09 20:03:03 +0530

    added solution for max product and value from array

---
+++ src/com/java/logicbuilding/program/MaxProductFromArrayKeyAndValue.java
@@ -0,0 +1,44 @@
+package com.java.logicbuilding.program;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class MaxProductFromArrayKeyAndValue {
+	
+	public static void main(String[] args) {
+		
+		String [] input = {"xyz 9","abc 21","xyz 21"};
+		
+
+		Map<String , Integer> map = new HashMap<String, Integer>();
+		
+		for (String data : input) {
+			
+			String[] product = data.split(" ");
+			String productName = product[0];
+			int productValue = Integer.parseInt(product[1]);
+			map.put(productName, map.getOrDefault(productName, 0)+productValue);
+			
+		}
+		
+		System.out.println(map);
+		
+		int maxValue = 0;
+		String product = null;
+		
+		
+		for(Entry<String , Integer> entrySet :map.entrySet()) {
+			if(maxValue<entrySet.getValue()) {
+				maxValue=entrySet.getValue();
+				product = entrySet.getKey();
+			}
+		}
+		
+		System.out.println(product + " : " + maxValue);
+		
+				
+		
+	}
+
+}
